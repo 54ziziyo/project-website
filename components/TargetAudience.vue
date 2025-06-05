d0a4c65630bd61dbe01638d5fb3b7d311b0f4a75
ziyun <54ziziyo@gmail.com>
2025-06-06 03:09:43 +0800

    新增 # 顧客回饋

--- components/TargetAudience.vue
+++ components/TargetAudience.vue
@@ -1,6 +1,42 @@
-<script lang="ts" setup></script>
-<template>
-  <div>目標客群</div>
-</template>
+<script setup lang="ts">
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+</script>
 
-<style></style>
+<template>
+  <div class="w-full overflow-hidden px-4">
+    <Carousel
+      class="w-full"
+      :opts="{
+        align: 'start',
+        slidesToScroll: 4,
+        loop: true,
+      }"
+    >
+      <CarouselContent class="-ml-0">
+        <CarouselItem
+          v-for="(_, index) in 10"
+          :key="index"
+          class="pl-2 basis-1/4"
+        >
+          <div class="p-1">
+            <Card>
+              <CardContent
+                class="flex aspect-square items-center justify-center p-6"
+              >
+                <span class="text-2xl font-semibold">{{ index + 1 }}</span>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  </div>
+</template>
